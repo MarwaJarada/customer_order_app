781dea661c3350cfa937c48f0853139afc61d749
Your-Full-Name <jaradamarwa@gmail.com>
2019-05-04 21:27:36 +0300

    add cancel event

--- src/controllers/DeleteCustomerController.java
+++ src/controllers/DeleteCustomerController.java
@@ -3,11 +3,15 @@ package controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+import static commonFunctions.GeneralCommonFunctions.closeScene;
 
 public class DeleteCustomerController {
-    @FXML private TableView deleteCustomerTable;
+    @FXML private AnchorPane customerDelete;
 
     public void cancel(ActionEvent event) {
+        closeScene(customerDelete);
     }
 
     public void deleteCustomer(ActionEvent event) {
--- src/controllers/SearchProductController.java
+++ src/controllers/SearchProductController.java
@@ -13,5 +13,6 @@ public class SearchProductController {
     }
 
     public void cancel(ActionEvent event) {
+
     }
 }

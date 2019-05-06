1977d943ad70c933491ba53baccb8af9a9757c4f
Your-Full-Name <jaradamarwa@gmail.com>
2019-05-06 15:53:55 +0300

    add function to show customer data into table view

--- src/commonFunctions/CustomerCommonFunctions.java
+++ src/commonFunctions/CustomerCommonFunctions.java
@@ -1,9 +1,14 @@
 package commonFunctions;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
+import entity.Customer;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.SQLException;
+
+import static db.CustomerDBOperation.showCustomer;
 
 public class CustomerCommonFunctions {
 
@@ -31,5 +36,19 @@ public class CustomerCommonFunctions {
                 System.out.println("Pressed OK.");
             }
         });
+    }
+
+    public static void refreshCustomers(TableView tableView,TableColumn idCol, TableColumn fnameCol, TableColumn lnameCol,
+                                        TableColumn mobileCol, TableColumn emailCol, TableColumn addressCol,
+                                        TableColumn genderCol) throws SQLException, ClassNotFoundException {
+        ObservableList<Customer> customers=showCustomer();
+        idCol.setCellValueFactory(new PropertyValueFactory<String,Customer>("id"));
+        fnameCol.setCellValueFactory(new PropertyValueFactory<String,Customer>("fname"));
+        lnameCol.setCellValueFactory(new PropertyValueFactory<String,Customer>("lname"));
+        mobileCol.setCellValueFactory(new PropertyValueFactory<String,Customer>("mobile"));
+        emailCol.setCellValueFactory(new PropertyValueFactory<String,Customer>("email"));
+        addressCol.setCellValueFactory(new PropertyValueFactory<String,Customer>("address"));
+        genderCol.setCellValueFactory(new PropertyValueFactory<String,Customer>("gender"));
+        tableView.setItems(customers);
     }
 }

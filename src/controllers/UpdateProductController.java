846eaa597457438392afe072a83da91f95274d62
Your-Full-Name <jaradamarwa@gmail.com>
2019-05-07 20:29:39 +0300

    select product grom table view and show into into text fields to update the product

--- src/controllers/UpdateProductController.java
+++ src/controllers/UpdateProductController.java
@@ -1,15 +1,18 @@
 package controllers;
 
 import commonFunctions.ProductCommonFunction;
+import entity.Product;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.Menu;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
-import java.awt.*;
+import javafx.scene.control.MenuItem;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -23,12 +26,13 @@ public class UpdateProductController implements Initializable {
     @FXML private TextField quantityTxtField;
     @FXML private TextArea descriptionTxtArea;
     @FXML private MenuButton categoryMenuBtn;
-    @FXML private TableView updateProductTable;
+    @FXML private TableView<Product> updateProductTable;
     @FXML private TableColumn nameCol;
     @FXML private TableColumn categoryCol;
     @FXML private TableColumn quantityCol;
     @FXML private TableColumn priceCol;
     @FXML private TableColumn descriptionCol;
+
 
     public void cancel(ActionEvent event) {
         closeScene(productUpdate);
@@ -38,11 +42,23 @@ public class UpdateProductController implements Initializable {
     }
 
     public void updateProduct(ActionEvent event) throws SQLException, ClassNotFoundException {
-        ProductCommonFunction.
-                refreshProducts(updateProductTable,nameCol,categoryCol,quantityCol,priceCol,descriptionCol);
+        if (updateProductTable.getSelectionModel().getSelectedItems().get(0)!=null) {
+            ObservableList<Product> product= updateProductTable.getSelectionModel().getSelectedItems();
+            nameTxtField.setText(product.get(0).getName());
+            nameTxtField.setEditable(false);
+            priceTxtField.setText(String.valueOf(product.get(0).getPrice()));
+            quantityTxtField.setText(String.valueOf(product.get(0).getQuantity()));
+            categoryMenuBtn.setText(product.get(0).getCategory());
+            descriptionTxtArea.setText(product.get(0).getDescription());
+
+        }
+
+
     }
 
-    public void refreshProducts(ActionEvent event) {
+    public void refreshProducts(ActionEvent event) throws SQLException, ClassNotFoundException {
+        ProductCommonFunction.
+                refreshProducts(updateProductTable,nameCol,categoryCol,quantityCol,priceCol,descriptionCol);
     }
 
     @Override
@@ -55,5 +71,11 @@ public class UpdateProductController implements Initializable {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public void chooseCategory(ActionEvent event) {
+        MenuItem selected = (MenuItem)event.getSource();
+        String itemSelected=selected.getText();
+        categoryMenuBtn.setText(itemSelected);
     }
 }
